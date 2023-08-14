5f936dbbf0e2b54446081ba582dd2fb8414ba39c
Khac Vy <khacvy93@gmail.com>
2023-08-14 21:15:20 +0700

    enable analytics

--- app/providers.tsx
+++ app/providers.tsx
@@ -8,7 +8,7 @@ import { Analytics } from "@vercel/analytics/react"
 import { SessionProvider, signOut, useSession } from "next-auth/react"
 import { ReactNode, useEffect, useMemo } from "react"
 import { useGumSDK } from "@/hooks/use-gum-sdk"
-import { IS_PROD, SOLANA_CLUSTER, SOLANA_PRC } from "@/utils/env"
+import { SOLANA_CLUSTER, SOLANA_PRC } from "@/utils/env"
 
 const UploadProviderWraper = ({ children }: { children: ReactNode }) => {
   const { connection } = useConnection()
@@ -69,7 +69,7 @@ export default function Providers({ children }: { children: ReactNode }) {
           <UploadProviderWraper>
             <SessionProvider>
               <Wrapper>{children}</Wrapper>
-              {IS_PROD && <Analytics />}
+              <Analytics />
             </SessionProvider>
           </UploadProviderWraper>
         </WalletModalProvider>
