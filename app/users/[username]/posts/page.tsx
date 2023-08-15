4ce6eeaf3074d43669d696c0ba074a59322e4abb
HieuNV <hieu26102000@gmail.com>
2023-08-15 14:52:10 +0700

    Update logic query posts on user page

--- app/users/[username]/posts/page.tsx
+++ app/users/[username]/posts/page.tsx
@@ -1,37 +1,17 @@
 import { notFound } from "next/navigation"
 import Supabase from "@/lib/supabase"
-import { constructMetadata } from "@/utils/meta"
 import Post from "../../../../components/user-page/post"
 
-export async function generateMetadata({ params }: { params: { username: string } }) {
-  const user = await Supabase.findUserUsername(params.username)
-  if (!user) {
-    return constructMetadata({
-      title: "Page not found",
-    })
-  }
+export default async function PostPage({ params }: { params: { username: string } }) {
+  const creator = await Supabase.findUserUsername(params.username)
 
-  // @ts-ignore
-  const title = `${user?.profile_metadata?.name ?? user?.domain_name}`
-  const description = `Say gm to ${title}`
-
-  return constructMetadata({
-    title,
-    description,
-    image: `/api/og?username=${user.domain_name}`,
-  })
-}
-
-export default async function UserPage({ params }: { params: { username: string } }) {
-  const user = await Supabase.findUserUsername(params.username)
-
-  if (!user) {
+  if (!creator) {
     notFound()
   }
 
   return (
     <div className="mt-10 flex w-full flex-col gap-4 md:gap-6 lg:flex-row lg:gap-10">
-      <Post user={user} />
+      <Post creator={creator} />
     </div>
   )
 }
